8b2610cc31162b9ebb547aca0ddf2dad05bc3ac1
cheezeypotatoes <curt8769@gmail.com>
2017-04-12 15:08:04 -0600

    feat (main) Add catalog table, shirt selector and remover

--- app/reducer.js
+++ app/reducer.js
@@ -1,13 +1,25 @@
 module.exports = function reducer(state, action) {
-  return [
-    ...state,
-    {
-    id: state[state.length - 1] ?
-      state[state.length - 1].id + 1 : 1,
+  switch(action.type) {
+    case 'SHIRT@ADD':
+      return [
+        ...state,
+        {
 
-    color: action.color,
-    size: action.size,
-    motto: action.motto
-  }]
+        id: state[state.length - 1] ?
+          state[state.length - 1].id + 1 : 1,
 
+        color: action.color,
+        size: action.size,
+        motto: action.motto
+
+        }]
+      break
+    case 'SHIRT@REMOVE':
+      return state.filter( shirt => shirt.id !== action.id)
+      break
+    default:
+    return state
+
+
+  }
 }
