a871d3a037448470b5c83e2ec8a73111b273534d
cheezeypotatoes <curt8769@gmail.com>
2017-04-13 10:35:04 -0600

    feat (store) Add shirt selector logic

--- app/index.js
+++ app/index.js
@@ -1,1 +1,24 @@
+import { createStore } from 'redux'
+import reducer from './reducer'
+
 console.log('hello world');
+
+const store = createStore( reducer )
+
+console.log(store);
+
+const newShirtDiv = document.querySelector('#createShirt')
+newShirtDiv.querySelector('#createButton').onclick = () => {
+  store.dispatch({
+      type: 'SHIRT@ADD',
+      color: newShirtDiv.querySelector('#color').value,
+      size: newShirtDiv.querySelector('#size').value,
+      motto: newShirtDiv.querySelector('#motto').value
+    })
+}
+
+console.log(store.getState());
+
+store.subscribe( () => {
+  console.log("store.getState():", store.getState());
+})
